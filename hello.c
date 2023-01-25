c61ae79c1a5d6312c2fdc49bcab6e966cb538a45
Taylor Slade <taylorslade@ku.edu>
2023-01-25 09:15:16 -0600

    Added hello-world program.

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+Taylor Slade
+KU ID: 3079517
+date created: 1/25/2023
+last modified: 1/25/2023
+purpose: hello world in C 
+*/
+
+#include <stdio.h>
+int main()
+{
+	printf("%s\n", "hello world");
+	return 0;
+}
